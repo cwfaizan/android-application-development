0754eb0473824d2eca86499c1ec1d4975908a28a
Programming with Faizan <73745377+ProgrammingWithFaizan@users.noreply.github.com>
2020-10-31 19:21:07 +0500

    Create TestActivity.java

---
+++ Design Customize Button/TestActivity.java
@@ -0,0 +1,28 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.pf.firstproject.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    Button buttonTestActivityClickMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        buttonTestActivityClickMe = findViewById(R.id.buttonTestActivityClickMe);
+
+        buttonTestActivityClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // put your code here
+                Toast.makeText(TestActivity.this, "Hello Programming with Faizan", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
