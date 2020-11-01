8101cbc6e84138ed2ba253fa9926e2cb36294e12
Programming with Faizan <73745377+ProgrammingWithFaizan@users.noreply.github.com>
2020-11-01 23:18:09 +0500

    Update MainActivity.java

--- Customize RadioButton/MainActivity.java
+++ Customize RadioButton/MainActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
     RadioButton radioButtonMainActivityMale, radioButtonMainActivityFemale;
     RadioButton radioButtonMainActivityOther;
+    RadioGroup radioGroupMainActivityGender;
     Button buttonMainActivityGetGender;
 
     @Override
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         radioButtonMainActivityMale = findViewById(R.id.radioButtonMainActivityMale);
         radioButtonMainActivityFemale = findViewById(R.id.radioButtonMainActivityFemale);
         radioButtonMainActivityOther = findViewById(R.id.radioButtonMainActivityOther);
+
+        radioGroupMainActivityGender = findViewById(R.id.radioGroupMainActivityGender);
         buttonMainActivityGetGender = findViewById(R.id.buttonMainActivityGetGender);
 
         // Get State of RadioButton(s) on Button onClick
@@ -49,23 +55,33 @@ public class MainActivity extends AppCompatActivity {
         radioButtonMainActivityMale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Male is checked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Male is checked (using RadioButton)", Toast.LENGTH_SHORT).show();
             }
         });
+        
+        // Get State of RadioButton on RadioGroup onClick (Method 2)
+        radioGroupMainActivityGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedRadioButtonId) {
+                switch (checkedRadioButtonId)
+                {
+                    case R.id.radioButtonMainActivityFemale:
+                        // do your coding
+                        Toast.makeText(MainActivity.this, "Female is checked (using RadioGroup)", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+        
     }
 
-    // Call Function onClick and Get State of RadioButton on RadioButton itself onClick (Method 2)
+    // Call Function onClick and Get State of RadioButton on RadioButton XML onClick (Method 3)
     public void radioButtonOnClickListenerMainActivity(@NotNull View view){
         switch (view.getId())
         {
-            case R.id.radioButtonMainActivityFemale:
-                // do your coding
-                Toast.makeText(MainActivity.this, "Female is checked", Toast.LENGTH_SHORT).show();
-                break;
-            
             case R.id.radioButtonMainActivityOther:
                 // do your coding
-                Toast.makeText(MainActivity.this, "Other is checked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Other is checked (using Function)", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
