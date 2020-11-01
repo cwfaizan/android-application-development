62ea3f2685fccb97960352050d8d4b5b8ae3f4b2
Programming with Faizan <73745377+ProgrammingWithFaizan@users.noreply.github.com>
2020-11-01 13:11:00 +0500

    Update TestActivity.java

--- Design Customize Button/TestActivity.java
+++ Design Customize Button/TestActivity.java
@@ -1,10 +1,12 @@
+package com.pf.tutorial;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.pf.firstproject.R;
+import com.pf.tutorial.R;
 
 public class TestActivity extends AppCompatActivity {
 
