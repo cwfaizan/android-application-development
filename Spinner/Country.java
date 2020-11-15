731f6e4b5b9a366044abc25fe83a7a3b9a7dcf21
Programming with Faizan <73745377+ProgrammingWithFaizan@users.noreply.github.com>
2020-11-15 23:34:29 +0500

    Create Country.java

---
+++ Spinner/Country.java
@@ -0,0 +1,33 @@
+package com.pf.tutorial;
+
+public class Country {
+
+    private int code;
+    private String name;
+
+    public Country(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
