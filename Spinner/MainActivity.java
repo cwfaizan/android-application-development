0bfb851a04568d7a26d65c26201ae8380e6545e2
Programming with Faizan <73745377+ProgrammingWithFaizan@users.noreply.github.com>
2020-11-15 23:33:00 +0500

    Update MainActivity.java

--- Spinner/MainActivity.java
+++ Spinner/MainActivity.java
@@ -1,7 +1,6 @@
 package com.pf.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,35 +14,36 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spinnerMainActivityCity;
-    Button buttonMainActivityGetCity;
-    ArrayList<String> arrayListCity = new ArrayList();
+    private Button buttonSubmit;
+    private Spinner spinnerCity, spinnerCountry;
+    private ArrayList<String> arrayListCity = new ArrayList<>();
+    private ArrayList<Country> arrayListCountry = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinnerMainActivityCity = findViewById(R.id.spinnerMainActivityCity);
-        buttonMainActivityGetCity = findViewById(R.id.buttonMainActivityGetCity);
+        spinnerCity = findViewById(R.id.spinnerMainActivityCity);
+        spinnerCountry = findViewById(R.id.spinnerMainActivityCountry);
+        buttonSubmit = findViewById(R.id.buttonMainActivitySubmit);
 
-        ArrayAdapter<String> adapterCity = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayListCity);
-        adapterCity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerMainActivityCity.setAdapter(adapterCity);
+        citySpinnerHandling();
+        /* Pass Country Object with Country Id or Country Code to Spinner */
+        countrySpinnerHandling();
+        formSubmissionHandling();
+    }
 
-        buttonMainActivityGetCity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String selectedCity = spinnerMainActivityCity.getSelectedItem().toString();
-                Toast.makeText(MainActivity.this, "Selected City is "+selectedCity, Toast.LENGTH_LONG).show();
-            }
-        });
+    private void citySpinnerHandling(){
+        ArrayAdapter<String> arrayAdapterCity = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, arrayListCity);
+        arrayAdapterCity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCity.setAdapter(arrayAdapterCity);
 
-        spinnerMainActivityCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                String selectedCity = adapterView.getItemAtPosition(position).toString();
-                Toast.makeText(MainActivity.this, "Selected City is "+selectedCity, Toast.LENGTH_LONG).show();
+                String selectedCity = adapterView.getSelectedItem().toString();
+                Toast.makeText(MainActivity.this, selectedCity, Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -52,7 +52,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Fill data and notify Adapter
         arrayListCity.addAll( Arrays.asList("Islamabad", "Rawalpindi", "Lahore", "Karachi", "Multan", "DG Khan") );
-        adapterCity.notifyDataSetChanged();
+        arrayAdapterCity.notifyDataSetChanged();
+    }
+
+    private void countrySpinnerHandling(){
+        ArrayAdapter<Country> arrayAdapterCountry = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, arrayListCountry);
+        arrayAdapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCountry.setAdapter(arrayAdapterCountry);
+
+        spinnerCountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Country country = (Country) adapterView.getSelectedItem();
+                Toast.makeText(MainActivity.this, country.getName()+" "+country.getCode(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // Fill data and notify Adapter
+        Country countryPakistan = new Country(92, "Pakistan");
+        arrayListCountry.add(countryPakistan);
+        arrayListCountry.add(new Country(1, "USA"));
+        arrayListCountry.add(new Country(91, "India"));
+        arrayAdapterCountry.notifyDataSetChanged();
+    }
+
+    private void formSubmissionHandling(){
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String selectedCity = spinnerCity.getSelectedItem().toString();
+                Toast.makeText(MainActivity.this, "Selected City is "+selectedCity, Toast.LENGTH_LONG).show();
+                Country selectedCountry = (Country) spinnerCountry.getSelectedItem();
+                Toast.makeText(MainActivity.this, "Selected Country is "+selectedCountry.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
