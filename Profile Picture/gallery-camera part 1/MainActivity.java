1686711bc97cfae2511ca5a5646f037be447994f
Programming with Faizan <73745377+ProgrammingWithFaizan@users.noreply.github.com>
2020-12-27 00:53:32 +0500

    Create MainActivity.java

---
+++ Profile Picture/gallery-camera part 1/MainActivity.java
@@ -0,0 +1,126 @@
+package com.pf.tutorial;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageViewProfilePicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageViewProfilePicture = findViewById(R.id.imageViewMainActivityProfilePicture);
+
+        imageViewProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseProfilePicture();
+            }
+        });
+    }
+
+    private void chooseProfilePicture(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_dialog_profile_picture, null);
+        builder.setCancelable(false);
+        builder.setView(dialogView);
+
+        ImageView imageViewADPPCamera = dialogView.findViewById(R.id.imageViewADPPCamera);
+        ImageView imageViewADPPGallery = dialogView.findViewById(R.id.imageViewADPPGallery);
+
+        AlertDialog alertDialogProfilePicture = builder.create();
+        alertDialogProfilePicture.show();
+
+        imageViewADPPCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkAndRequestPermissions()) {
+                    takePictureFromCamera();
+                    alertDialogProfilePicture.dismiss();
+                }
+            }
+        });
+
+        imageViewADPPGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePictureFromGallery();
+                alertDialogProfilePicture.dismiss();
+            }
+        });
+    }
+
+    private void takePictureFromGallery(){
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto, 1);
+    }
+
+    private void takePictureFromCamera(){
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePicture.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePicture, 2);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImageUri = data.getData();
+                    imageViewProfilePicture.setImageURI(selectedImageUri);
+                }
+                break;
+            case 2:
+                if(resultCode == RESULT_OK){
+                    Bundle bundle = data.getExtras();
+                    Bitmap bitmapImage = (Bitmap) bundle.get("data");
+                    imageViewProfilePicture.setImageBitmap(bitmapImage);
+                }
+                break;
+        }
+    }
+
+    private boolean checkAndRequestPermissions(){
+        if(Build.VERSION.SDK_INT >= 23){
+            int cameraPermission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+            if(cameraPermission == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 20);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 20 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            takePictureFromCamera();
+        }
+        else
+            Toast.makeText(MainActivity.this, "Permission not Granted", Toast.LENGTH_SHORT).show();
+    }
+}
